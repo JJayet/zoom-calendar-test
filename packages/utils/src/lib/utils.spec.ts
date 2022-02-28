727931159f342263208bfa9ff940ab426ee4e7dc
Jonathan Jayet <github@jayet.io>
2022-02-28 15:51:39 +0100

    test(utils): test addMinutes to date

--- packages/utils/src/lib/utils.spec.ts
+++ packages/utils/src/lib/utils.spec.ts
@@ -1,7 +1,9 @@
-import { utils } from './utils'
+import { addMinutesToDate } from './utils'
 
-describe('utils', () => {
-  it('should work', () => {
-    expect(utils()).toEqual('utils')
+describe('addMinutesToDate', () => {
+  it('should correctly add minutes to a date', () => {
+    const initialDate = new Date('2022-01-01T00:00:00.000Z')
+    const newDate = addMinutesToDate(initialDate, 90)
+    expect(newDate).toEqual(new Date('2022-01-01T01:30:00.000Z'))
   })
 })
